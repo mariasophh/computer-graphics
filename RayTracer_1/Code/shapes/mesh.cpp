7795651651e21a799196d520db44bd0191f45775
Anda-Amelia <andamelia@yahoo.com>
2020-02-22 12:26:10 +0100

    Not working - rotation

--- RayTracer_1/Code/shapes/mesh.cpp
+++ RayTracer_1/Code/shapes/mesh.cpp
@@ -14,8 +14,20 @@ Hit Mesh::intersect(Ray const &ray)
 {
     // Replace the return of a NO_HIT by determining the intersection based
     // on the ray and this class's data members.
+    Hit min_hit(numeric_limits<double>::infinity(), Vector());
+    ObjectPtr obj = nullptr;
+    for (unsigned idx = 0; idx != d_tris.size(); ++idx) {
+        Hit hit(d_tris[idx]->intersect(ray));
+        if (hit.t < min_hit.t)
+        {
+            min_hit = hit;
+            obj = d_tris[idx];
+        }
+    }
 
-    return Hit::NO_HIT();
+    // No hit? Return background color.
+    if (!obj) return Hit::NO_HIT();
+    return Hit(min_hit);
 }
 
 Mesh::Mesh(string const &filename, Point const &position, Vector const &rotation, Vector const &scale)
@@ -38,15 +50,36 @@ Mesh::Mesh(string const &filename, Point const &position, Vector const &rotation
         // of the triangle (v0, v1, and v2) here.
 
         // Non-uniform scaling
-//        v0 = ...;
-//        v1 = ...;
-//        v2 = ...;
+        v0 = v0 * scale;
+        v1 = v1 * scale;
+        v2 = v2 * scale;
 
         // Rotation
-        // ...
+        // Rotation around the x axis
+        double cosine = cos(rotation.x);
+        double sine = sin(rotation.x);
+        v0 = Point(v0.x, v0.y*cosine - v0.z*sine, v0.y*sine + v0.z*cosine);
+        v1 = Point(v1.x, v1.y*cosine - v1.z*sine, v1.y*sine + v1.z*cosine);
+        v2 = Point(v2.x, v2.y*cosine - v2.z*sine, v2.y*sine + v2.z*cosine);
+
+        // Rotation around y axis
+        cosine = cos(rotation.y);
+        sine = sin(rotation.y);
+        v0 = Point(v0.x*cosine + v0.z*sine, v0.y, -v0.x*sine + v0.z*cosine);
+        v1 = Point(v1.x*cosine + v1.z*sine, v1.y, -v1.x*sine + v1.z*cosine);
+        v2 = Point(v2.x*cosine + v2.z*sine, v2.y, -v2.x*sine + v2.z*cosine);
+
+        // Rotation around z axis
+        cosine = cos(rotation.z);
+        sine = sin(rotation.z);
+        v0 = Point(v0.x*cosine - v0.y*sine, v0.x*sine + v0.y*cosine, v0.z);
+        v1 = Point(v1.x*cosine - v1.y*sine, v1.x*sine + v1.y*cosine, v1.z);
+        v2 = Point(v2.x*cosine - v2.y*sine, v2.x*sine + v2.y*cosine, v0.z);
 
         // Translation
-        // ...
+        v0 = (v0 + position);
+        v1 = (v1 + position);
+        v2 = (v2 + position);
 
         d_tris.push_back(ObjectPtr(new Triangle(v0, v1, v2)));
     }
--- RayTracer_1/Code/shapes/triangle.cpp
+++ RayTracer_1/Code/shapes/triangle.cpp
@@ -3,69 +3,15 @@
 #include <cmath>
 
 Hit Triangle::intersect(Ray const &ray)
-{
-    /*
-    // If the dot product of N and D is 0, then the ray is parallel to
-    // the plane => no intersection
-    if (N.dot(ray.D) == 0) return Hit::NO_HIT();
-
-    double t = (N.dot(v0) - N.dot(ray.O)) / N.dot(ray.D);
-
-    // Plane lies behind ray origin
-    if (t < 0) return Hit::NO_HIT();
-
-    // Ray and plane intersection point (p)
-    Point p = (ray.O + t*ray.D).normalized();
-
-    // Check if intersection point is inside the triangle
-    // p and v0 on same side of v1v2, p and v1 on same side of v0v2, p and v2 on same side of v0v1
-    Triple cross1 = p.cross(v0).normalized();
-    Triple cross2 = v1.cross(v2).normalized();
-
-    Triple cross3 = p.cross(v1).normalized();
-    Triple cross4 = v0.cross(v2).normalized();
-
-    Triple cross5 = p.cross(v2).normalized();
-    Triple cross6 = v0.cross(v1).normalized();
-
-    if (cross1.dot(cross2) > 0 && cross3.dot(cross4) >0 && cross5.dot(cross6) >0) {
-        return Hit(t,N);
-    } else {
-        return Hit::NO_HIT();
-    }
-
-    // Normal: directed towards ray origin
-    N = (N.dot(ray.D) < 0) ? N : -N;
-    */
-
-
-    // Check if intersection point is within triangle: A = A1 + A2 + A3
-    // Area of triangle = half of parallelogram's area (magnitude of cross product)
-    // A = 1/2*|v0v1 x v0v2|
-    /*double area = fabs((v1-v0).cross(v2-v0)) / 2;
-    double area_1 = fabs((v1-intersection).cross(v2-intersection)) / 2;
-    double area_2 = fabs((v1-intersection).cross(v0-intersection)) / 2;
-    double area_3 = fabs((v2-intersection).cross(v0-intersection)) / 2;
-
-    if (area == area_1 + area_2 + area_3) {
-        return Hit(t,N);
-    } else {
-        return Hit::NO_HIT();
-    }*/
-
-    // If the dot product of N and D is close to 0, then the ray is parallel to
-    // the plane => no intersection
+{    // Implementation of Möller-Trumbore algorithm
     double eps = std::numeric_limits<double>::epsilon();
-    if (fabs(N.dot(ray.D)) < eps) return Hit::NO_HIT();
-
-    // Implementation of Möller-Trumbore algorithm
     double t, u, v;
 
     // Edges of the trianlge (with vertices v0, v1, v2)
     Triple edge1 = v1-v0;
     Triple edge2 = v2-v0;
 
-    // Corss product between D and edge 2
+    // Corss product between vector direction D and edge 2
     Vector h = ray.D.cross(edge2);
     // Calculate determinant
     double det = (h).dot(edge1);
@@ -76,6 +22,7 @@ Hit Triangle::intersect(Ray const &ray)
 
     // Vector from first vertex of triangle to origin of ray: s
     Triple s = ray.O - v0;
+    // Cross product between triangle's first edge and s: q
     Triple q = s.cross(edge1);
 
     // Calculate u, v and t
@@ -85,6 +32,7 @@ Hit Triangle::intersect(Ray const &ray)
     v = ray.D.dot(q) * inv_det;
     if (v < 0.0 || v + u > 1.0) return Hit::NO_HIT();
 
+    // Calculate intersection point
     t = edge2.dot(q) * inv_det;
 
     // Normal: directed towards ray origin
@@ -95,8 +43,6 @@ Hit Triangle::intersect(Ray const &ray)
     } else {
         return Hit::NO_HIT();
     }
-
-
 }
 
 Triangle::Triangle(Point const &v0,
