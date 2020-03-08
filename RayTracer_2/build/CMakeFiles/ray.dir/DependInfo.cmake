9c8052a7f6f127e9fea33a86cdbe495ec5495783
Maria Sophia Stefan <mariasophia.stefan@yahoo.com>
2020-03-08 14:23:41 +0100

    raytracer 2 kick-off

---
+++ RayTracer_2/build/CMakeFiles/ray.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/image.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/image.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/lode/lodepng.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/lode/lodepng.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/main.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/main.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/objloader.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/objloader.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/raytracer.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/raytracer.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/scene.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/scene.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/shapes/quad.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/shapes/quad.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/shapes/solvers.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/shapes/solvers.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/shapes/sphere.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/shapes/sphere.cpp.o"
+  "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/src/triple.cpp" "/Users/mariasophh/desktop/computer-graphics/RayTracer_2/build/CMakeFiles/ray.dir/src/triple.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
